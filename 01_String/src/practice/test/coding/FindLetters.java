ff9e61d6528f7d9255de520090784d485ae484a6
ansoohyeon <dkstngus0615@gmail.com>
2023-11-22 18:05:48 +0900

    01-01(문자 찾기) 풀이

---
+++ 01_String/src/practice/test/coding/FindLetters.java
@@ -0,0 +1,33 @@
+package practice.test.coding;
+
+import java.util.Scanner;
+
+public class FindLetters {
+
+	public int solution(String str, char t) {
+		
+		int answer = 0;
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+
+		for(char x : str.toCharArray()) {
+			if(x==t) answer++;
+		};
+
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+		FindLetters T = new FindLetters();
+		
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		
+		System.out.println(T.solution(str, c));
+		
+	}
+
+}
